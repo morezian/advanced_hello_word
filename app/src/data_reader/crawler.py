6bfa9d9e91212807057e2c35ce5f7e10fffdbbcb
Mohammadreza Tavakolian <tavakolian.mr@gmail.com>
2020-12-25 20:14:04 +0330

    crawl empty list return bug fixed - add new inputs to buysellstatus objects

--- app/src/data_reader/crawler.py
+++ app/src/data_reader/crawler.py
@@ -1,8 +1,8 @@
 import requests
-from dataclasses import dataclass
 from app.src.loggers.file_logger import logger
 from app.src.stock.stock import BuySellStatus
-from collections import OrderedDict
+import time
+from datetime import datetime
 
 names = {'UNK':  3,
          'first_price': 4 ,
@@ -32,12 +32,18 @@ names = {'UNK':  3,
          'human_sell_vol': 28 ,
          'civil_sell_vol':29}
 
-@dataclass
 class Symbol:
-    name: str
-    latin_name: str
-    unique_id: str
-    current_buy_sell_status: BuySellStatus
+    def __init__(self,name,latin_name,unique_id,current_buy_sell_status):
+        self.name: str = name
+        self.latin_name: str = latin_name
+        self.unique_id: str = unique_id
+        self.current_buy_sell_status:BuySellStatus = current_buy_sell_status
+
+def get_today_market_opening_time():
+    return int(datetime(year=datetime.now().year,
+                                    month=datetime.now().month,
+                                    day=datetime.now().day,
+                                    hour=9, minute=0, second=0).timestamp())
 
 def crawl_data ()->list:
         table , symbols_human_civil_trading_status = __load_tables()
@@ -61,9 +67,9 @@ def __load_tables():
             symbols_human_civil_trading_status = requests.get('http://www.tsetmc.com/tsev2/data/ClientTypeAll.aspx')
             try:
                 watcher_table = watcher_table.text.split('@@')[1].split(';')
-                symbols_human_civil_trading_status = symbols_human_civil_trading_status.text.split(';')
             except:
                 watcher_table = watcher_table.text.split('0.2%,')[1].split(';')
+            symbols_human_civil_trading_status = symbols_human_civil_trading_status.text.split(';')
             break
         except Exception as e:
             logger.error(e)
@@ -77,9 +83,8 @@ def __get_union_of_two_tables(watcher_table: list,symbols_human_civil_trading_st
         if x[1][0] == 'I':
             watcher_table_dict[x[0]] = x[1:]
     symbols_human_civil_trading_status_dict = {}
-    [symbols_human_civil_trading_status_dict.update({x[0]: x[1:]}) for x in symbols_human_civil_trading_status if x[0] in watcher_table_dict.keys()]
+    [symbols_human_civil_trading_status_dict.update({str(x[0]): x[1:]}) for x in symbols_human_civil_trading_status if str(x[0]) in watcher_table_dict.keys()]
     for symbol in symbols_human_civil_trading_status_dict:
-        watcher_table_data = watcher_table_dict[symbol]
         symbols_human_civil_trading_status_dict[symbol] = watcher_table_dict[symbol] + symbols_human_civil_trading_status_dict[symbol]
     return symbols_human_civil_trading_status_dict
 
@@ -96,7 +101,10 @@ def __get_buy_sell_status(list_of_string_numbers) -> BuySellStatus:
                             trade_price= int(list_of_string_numbers[names.get('latest_traded_price')]),
                             vol= int(list_of_string_numbers[names.get('vol')]),
                             final_price= int(list_of_string_numbers[names.get('closed_price')]),
-                            first_traded_price= int(list_of_string_numbers[names.get('first_price')])
+                            first_trade= int(list_of_string_numbers[names.get('first_price')]),
+                            min_day_price=int(list_of_string_numbers[names.get('min_traded_price')]),
+                            max_day_price=int(list_of_string_numbers[names.get('max_traded_price')]),
+                            start_time_stamp= get_today_market_opening_time(),
+                            end_time_stamp= int(time.time())
                             )
     return new_bss
-crawl_data()
