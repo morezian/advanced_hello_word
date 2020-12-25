e4921ec9df88f273489792b2cb38a8ef1155ab8d
Mohammadreza Tavakolian <tavakolian.mr@gmail.com>
2020-12-26 01:08:40 +0330

    add max and min touched price to crawl response

--- app/src/data_reader/crawler.py
+++ app/src/data_reader/crawler.py
@@ -106,6 +106,8 @@ def __get_buy_sell_status(list_of_string_numbers) -> BuySellStatus:
                             min_day_price=float(list_of_string_numbers[names.get('min_valid_price')]),
                             max_day_price=float(list_of_string_numbers[names.get('max_valid_price')]),
                             start_time_stamp= get_today_market_opening_time(),
+                            max_day_touched_price= int(list_of_string_numbers[names.get('max_traded_price')]),
+                            min_day_touched_price= int(list_of_string_numbers[names.get('min_traded_price')]),
                             end_time_stamp= int(time.time())
                             )
     return new_bss
