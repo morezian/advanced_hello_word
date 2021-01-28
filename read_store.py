c09afb51527a1f9530b74e948bbd86a53e15dcd7
dehghani735 <dehghani735@gmail.com>
2021-01-29 01:22:48 +0330

    json message sample7

--- read_store.py
+++ read_store.py
@@ -53,7 +53,7 @@ async def handle(websocket, path):
         if WebSocketUtility.get_instance().WebSocketDict[websocket]:
             print('before sending')
             mm = WebSocketUtility.get_instance().get_stock_list()
-            result = '{ "response" : '  WebSocketUtility.get_instance().get_stock_list() + '}'
+            result = '{ "response" : ' + WebSocketUtility.get_instance().get_stock_list() + '}'
             #result = WebSocketUtility.get_instance().get_stock_list()
             await websocket.send(result)
             WebSocketUtility.get_instance().WebSocketDict[websocket] = False
