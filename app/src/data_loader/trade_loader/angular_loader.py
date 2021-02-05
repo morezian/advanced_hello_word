c91ee9e57a3a0e7bbff1fbaaf0c4c8c15aa351f7
dehghani735 <dehghani735@gmail.com>
2021-02-05 13:10:26 +0330

    already closed bug fixed and utf8 charset fixed

--- app/src/data_loader/trade_loader/angular_loader.py
+++ app/src/data_loader/trade_loader/angular_loader.py
@@ -14,43 +14,47 @@ class AngularLoader():
         self.__signal_type = signal_type
         self.__signal_count = 0
         print('Server Started')
-        self.connection = pymysql.connect(host='localhost',
-                             user='admin',
-                             password='vwB75K',
-                             database='trade_db',
-                             cursorclass=pymysql.cursors.DictCursor)
         
     def get_signal_count(self):
         return self.__signal_count
+    
+    def __insert_into_db(self, connection, stock_dict):
+        #with self.connection:
+        with connection.cursor() as cursor:
+            # Create a new record
+            sql = "INSERT INTO `daily_trades` (`name`) VALUES (%s)"
+            print('name ', stock_dict["name"])
+            cursor.execute(sql, (stock_dict["name"]))
+
+            # connection is not autocommit by default. So you must commit to save
+            # your changes.
+            connection.commit()
 
     def load_stock_list(self, stock_list):
         print(stock_list)
         self.__signal_count += len(stock_list)
         res_dict = []
-        for stock in stock_list:
-            response_stock = {**stock.to_dict(), **{"score_level": self.__signal_type}}
-            res_dict.append(response_stock)
+        connection = pymysql.connect(host='localhost', #79.175.176.165
+                             user='admin',
+                             password='vwB75K',
+                             database='trade_db',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+        with connection:
+            for stock in stock_list:
+                response_stock = {**stock.to_dict(), **{"score_level": self.__signal_type}}
+                self.__insert_into_db(connection, response_stock)
+                res_dict.append(response_stock)
         #WebSocketUtility.get_instance().set_send_status(True)
         #for i in WebSocketUtility.get_instance().WebSocketDict:
         #    #i.send(json.dumps(res_dict, sort_keys=True, indent=4))
         #    WebSocketUtility.get_instance().WebSocketDict[i] = True
         #WebSocketUtility.get_instance().set_stock_list(res_dict)
         
-        with self.connection:
-            """with self.connection.cursor() as cursor:
-                # Create a new record
-                sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-                cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
-
-                # connection is not autocommit by default. So you must commit to save
-                # your changes.
-                self.connection.commit()"""
-                
-            with self.connection.cursor() as cursor:
+        """ with self.connection.cursor() as cursor:
                 # Read a single record
                 sql = "SELECT version()"
                 cursor.execute(sql)
                 result = cursor.fetchone()
-                print(result)
-            
+                print(result)"""
         print('signaled')
