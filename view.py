f78080281bc83e8b18053f5f70394c15cf505957
dehghani735 <dehghani735@gmail.com>
2021-02-18 08:40:44 -0800

    cors fixed5

--- view.py
+++ view.py
@@ -11,3 +11,10 @@ api = Api(app)
 
 #api.add_resource(StatusInterface, '/status')
 api.add_resource(StockInfo, '/stockInfo')
+
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  return response
