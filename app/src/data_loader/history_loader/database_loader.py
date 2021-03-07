476d59310bc08a169fc87d20fe55109a4a7e8c25
Mohammadreza Tavakolian <tavakolian.mr@gmail.com>
2021-03-07 16:16:51 +0330

    hotfixed database_loader.py

--- app/src/data_loader/history_loader/database_loader.py
+++ app/src/data_loader/history_loader/database_loader.py
@@ -1,15 +1,14 @@
-import mysql.connector
+import mysql.connector as connector
 from app.src.interfaces.stock_history_interface import StockHistory
 from typing import *
 
-
-class MysqlConnector:
-    def __init__(self, host, username, password, database, port=3306):
-        self.connector = mysql.connector.connect(host=f"{host}", user=username, password=password, database=database
-                                                 )
-
-    def get_instance(self):
-        return self.connector.cursor()
+def connection(**config):
+    try:
+        c = connector.connect(**config)
+        return c
+    except Exception as e:
+        print (e)
+        exit(1)
 
 def _extract_db_format_data(datalist: Dict[str, StockHistory]):
     extracted_data_list = []
@@ -22,26 +21,29 @@ def _extract_db_format_data(datalist: Dict[str, StockHistory]):
             except AttributeError:
                 buy_sells = {**buy_sells, **{key: item[key] for item in symbol.buy_sell_status_list}}
         extracted_data_list.append({**symbol_dict, **buy_sells})
-        # for day in buy_sells:
-        #     extracted_data_list.append({**symbol_dict, **day})
-    # extracted_data_list += [{**symbol_dict, **row} for row in buy_sells]
     return extracted_data_list
 
 
 class HistoryMysqlLoader:
     def __init__(self, host, username, password, database, port=3306, history_table="history_tbl"):
-        self.curser = MysqlConnector(host=host, password=password, database=database, port=port,
-                                     username=username).get_instance()
+        self.connection = connection(host=host, password=password, database=database, port=port,
+                                     user=username)
         self.history_tbl = history_table
     def insert_stock_history_list_to_db(self, datalist: Dict[str, StockHistory], batch_size=1000):
         datalist = _extract_db_format_data(datalist)
+        row = []
+        curser = self.connection.cursor()
         for items in range(0, len(datalist), batch_size):
-            self.curser.executemany(f"""INSERT INTO {self.history_tbl} {tuple(history_table_schema.keys())}
-                                      VALUES {tuple(history_table_schema.values())}""", datalist)
+            query = f"""INSERT INTO history_tbl (`civil_buy_count`, `civil_buy_price`, `civil_buy_vol`, `civil_sell_count`, `civil_sell_price`, `civil_sell_vol`,`end_time_stamp`, `final_price`, `first_trade`, `human_buy_count`, `human_buy_vol`, `human_sell_count`, `human_sell_price`, `human_sell_vol`, `latin_name`, `market_cap`, `max_day_price`, `max_day_touched_price`, `min_day_price`, `min_day_touched_price`, `name`, `shares_count`, `start_time_stamp`, `trade_price`, `vol`)
+                                      value (%s, %s, %s, %s, %s, %s, %s,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+            for i in datalist:
+                d = {k: v for k, v in sorted(i.items(), key=lambda item: item[0])}
+                row.append(tuple(d.values()))
+            curser.executemany(query, row)
+            self.connection.commit()
 
-    def update_stock_history_list_in_db(self, datalist):
-        ...
-
+    def __del__(self):
+        self.connection.close()
 
 history_table_schema = {
     # "id"  : "%s",
@@ -72,4 +74,3 @@ history_table_schema = {
     "start_time_stamp": "%d",
 }
 history_table_schema = {k: v for k, v in sorted(history_table_schema.items(), key=lambda item: item[0])}
-# a = HistoryMysqlLoader(host="168.119.202.175",port=3309,username='admin',password='5}w:3M6%Wtv5sDe(')
